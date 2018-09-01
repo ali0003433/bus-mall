a1ee4dd0c8bf8332e0967219fcf1ae11cf12fea8
AL <alyssaliguori@Alyssas-MacBook-Air.local>
2018-09-01 16:59:12 -0400

    working code for 3 clicks

---
+++ chart.js
@@ -0,0 +1,48 @@
+//get canvas element in html w/ id 'my-chart'
+let elChart = document.getElementById('my-chart').getContext('2d');
+
+//define a function to populate the clicks and shown in the canvas element
+function populateChart(prop) {
+    //declare var and assign empty array to store property values
+    let propArray = [];
+    //loop through the imageArray 
+    for (let i =0; i < imageArray.length; i++) {
+    propArray.push(imageArray[i][prop])
+    }
+    return propArray;
+}
+
+//declare a new var and assign it the value of a new instance of chart object (using chartjs CDN)
+function displayChart() {
+    elImageContainer.innerHTML = '';
+    elChart.innerHTML = '';
+    let myChart = new Chart (elChart, {
+        //assign property of type to be a bar chart
+        type: 'bar',
+        //set up data property that contains the labels for data 
+        data: {
+            labels: populateChart('name'),
+            datasets: [
+                {
+                    label: 'Times clicked',
+                    data: populateChart('clicked'),
+                    backgroundColor: 'lightgreen'
+                }, 
+                {
+                    label: 'Times shown',
+                    data: populateChart('shown'),
+                    backgroundColor: 'grey'
+                }
+            ],
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        }
+    });
+}
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let imageArray = [];
 let firstImage; 
 let secondImage;
 let thirdImage; 
+let totalClicks = 0;
 
 //find element with id image-container using document.getElementById
 let elImageContainer = document.getElementById('image-container')
@@ -18,14 +19,16 @@ let FontImage = function(name, style, filePath, id) {
     this.id = id;
     this.shown = 0;
     this.clicked = 0;
-    this.addClicks;
 };
+
 //check if localStorage has any values
 if(localStorage.length > 0) {
     //retrieve stored imageArray from local storage because it has previous clicks and shown
     let getData = localStorage.getItem('storageImageArray')
     //reassign value of imageArray to the parsed version of localStorage imageArray
     imageArray = JSON.parse(getData);
+    let getClicks = localStorage.getItem('storageTotalclicks')
+    totalClicks = JSON.parse(getClicks)
 } else {
     //if no localStorage values, instantiate constructor to create multiple instances of font images
     let Garamond = new FontImage('Garamond','serif','img/garamond.jpg', 'garamond')
@@ -45,59 +48,58 @@ function randomImage() {
     //declare a variable imageIndex and assign it an image object with an index of randomNumber (calculated above)
     let imageArrIndexChosen = imageArray[randomNumber]
     //increment the shown property of the randomly chosen object (via imageArray[randomArray]) each time the object is displayed
-    imageArrIndexChosen.shown +=1
     //return the random image object
     return imageArrIndexChosen;
 }
 
 //define event handler function that will increment the property clicked if html img id is equal to the image object id
-function imageClicked(event){
-    //
+function imageClicked(event) {
+    //assign clicks to correct image
     if (event.target.id === firstImage.id) {
         firstImage.clicked += 1;
-    } else if (event.target.id === secondImage.id) {
+        } else if (event.target.id === secondImage.id) {
         secondImage.clicked += 1;
-    } else if (event.target.id === thirdImage.id) {
+        } else if (event.target.id === thirdImage.id) {
         thirdImage.clicked += 1; 
     }
-    //invoke displayImages to display 3 new images
-    displayImages();
+    totalClicks += 1;
+    localStorage.setItem('storageImageArray', JSON.stringify(imageArray))
+    localStorage.setItem('storageTotalClicks', JSON.stringify(totalClicks))
+    if (totalClicks === 3) {
+        displayChart()
+        localStorage.clear('storageImageArray')
+        } else {
     //every time an event is clicked, save imageArray to localStorage
-    localStorage.setItem('storageImageArray', JSON.stringify(imageArray));
-    //invoke chart function to display data for images clicked and shown
-    dsiplayChart()
+        displayImages();
+        }
 }
-
 //define a function that will loop three times. Assign the return value of randomImage to var imageObject. In each loop, assign imageObject a new randomIamge. (1) During first loop, assign the random image to global var firstImage. Create a new img element in html. append it to the div. Set the html attribute id to the id of the randomly chosen image. Set the source of the img element to the randomly chosen image's filePath property. Add an event listener to that img element so that it runs the function imageClicked each time an event occurs. (2) During second loop, assign the random image the global var secondImage. (3) During third loop, assign the random image the global var thirdImage. 
 function displayImages() {
     //reassign html div to empty string 
     elImageContainer.innerHTML = '';
     //create for loop to iterate three times to display images
-    for(let i = 0; i <3; i++){
+    for(let i = 0; i < 3; i++){
         let imageObject = randomImage();
         //declare variable imageObject and assign it to return value of randomImage which is the same as imageArrIndexChosen 
         //if first iteration, assign the random image chosen to firstImage 
         if( i === 0){
-            console.log(firstImage = imageObject)
+            firstImage = imageObject
         //if second iteration, assign the random image chosen to secondImage
         } else if (i === 1) {
-            while(imageObject.id === firstImage.id){
-                console.log('entering while',imageObject.id)
+            while(imageObject.id === firstImage.id){            
                 imageObject = randomImage();
-                console.log('new try while i=2',imageObject.id)
-                }
-            console.log(secondImage = imageObject);
+            }
+                secondImage = imageObject;
         //if anything other than first or second iteration, assign the random images chosen to thirdImage
             } else {
             while(imageObject.id === firstImage.id || imageObject.id === secondImage.id) {
-            imageObject = randomImage();
-            console.log('new try while i=3', imageObject.id)
+                imageObject = randomImage();
             }
-        console.log(thirdImage = imageObject);
+        thirdImage = imageObject;
         }
+    imageObject.shown +=1
     //create an img element in html
     let elImage = document.createElement('img');
-    console.log(elImage)
     //append new img element to the div with id 'image-container'
     elImageContainer.appendChild(elImage);
     //set id of new img element to be the id of that randomly chosen image 
@@ -110,5 +112,3 @@ function displayImages() {
 }
 //invoke display images function 
 displayImages();
-console.log(elImageContainer);
-console.log('images', firstImage, secondImage, thirdImage);
